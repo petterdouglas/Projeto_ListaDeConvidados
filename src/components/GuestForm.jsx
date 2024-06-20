0672f393ccff3b228c50065efd086fef7ca18390
Petter <petterdouglas7@gmail.com>
2024-06-20 19:07:28 -0300

    added new function - customInvitedFor

--- src/components/GuestForm.jsx
+++ src/components/GuestForm.jsx
@@ -5,15 +5,25 @@ const GuestForm = ({ guests, addGuest }) => {
 
     const [value, setValue] = useState('')
     const [invitedFor, setInvitedfor] = useState('')
+    const [customInvitedFor, setCustomInvitedfor] = useState('')
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        if(!value || !invitedFor) return
-        addGuest(value, invitedFor)
-        localStorage.setItem('guests', JSON.stringify(guests))
+        if (!value || !invitedFor) return
+        if (invitedFor === 'anotherOption') {
+            addGuest(value, customInvitedFor)
+            localStorage.setItem('guests', JSON.stringify(guests))
+        } else {
+            addGuest(value, invitedFor)
+            localStorage.setItem('guests', JSON.stringify(guests))
+        }
         //limpar inputs
         setValue("")
         setInvitedfor("")
+    }
+
+    const handleCustomInvitedFor = () => {
+        return <div><input type="text" placeholder="Insira a pessoa que convidou..." onChange={(e) => setCustomInvitedfor(e.target.value)} value={customInvitedFor} /></div>
     }
 
     return (
@@ -24,8 +34,9 @@ const GuestForm = ({ guests, addGuest }) => {
                 <select onChange={(e) => setInvitedfor(e.target.value)} value={invitedFor}>
                     <option key="-1" value="">Convidado por</option>
                     {guests.map((list) => (<option key={list.id} value={list.guest}>{list.guest}</option>))}
-                    <option key="-2" value="Ninguém"></option>
+                    <option key="-2" value="anotherOption">Inserir outra pessoa</option>
                 </select>
+                {invitedFor === "anotherOption" ? handleCustomInvitedFor() : null}
                 <button type="submit">Adicionar</button>
             </form>
         </div>
